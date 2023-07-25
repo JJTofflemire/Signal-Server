dd2396f3b5f62ae2eb761015d149e210bdb29405
JJTofflemire <andyababcock@gmail.com>
2023-07-25 14:22:34 -0700

    fix: make docker filter function

--- filtered-docker-compose.sh
+++ filtered-docker-compose.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker compose up --no-log-prefix | awk '{gsub(/WARN /, "\033[33m&\033[0m"); gsub(/ERROR/, "\033[31m&\033[0m"); gsub(/INFO/, "\033[32m&\033[0m"); print}' | sed '/Timing: [0-9]\+ ms/,/<\/html>/d; /^$/d'
+docker-compose up --no-log-prefix | awk '{
+    gsub(/WARN /, "\033[33m&\033[0m");
+    gsub(/ERROR/, "\033[31m&\033[0m");
+    gsub(/INFO/, "\033[32m&\033[0m");
+}
+/Timing: [0-9]+ ms/,/<\/html>/ {next}
+!/^\s*$/ {
+    print
+}'
